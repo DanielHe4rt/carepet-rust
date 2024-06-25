d210c25ed806120d68b1a8d67c59b9aaf4f39457
danielhe4rt <hey@danielheart.dev>
2024-06-25 13:59:39 -0300

    wip

--- src/repositories/owner_repository.rs
+++ src/repositories/owner_repository.rs
@@ -1,40 +1,49 @@
 use std::sync::Arc;
 
 use anyhow::{anyhow, Result};
-use scylla::Session;
+use scylla::{prepared_statement::PreparedStatement, Session};
 use uuid::Uuid;
 
 use crate::model::owner::Owner;
 
+const INSERT_OWNER_QUERY: &str = r"INSERT INTO owners (owner_id, name, address) VALUES (?, ?, ?)";
+
+const FIND_OWNER_BY_PARTITION: &str = r"SELECT owner_id, name, address FROM owners WHERE owner_id = ?";
+
 pub struct OwnerRepository {
     session: Arc<Session>,
+    insert_owner_query: PreparedStatement,
+    find_owner_by_partition: PreparedStatement,
 }
 
 impl OwnerRepository {
     pub async fn new(session: Arc<Session>) -> Self {
-        Self { session }
+        let insert_owner_query = session.prepare(INSERT_OWNER_QUERY).await.unwrap();
+        let find_owner_by_partition = session.prepare(FIND_OWNER_BY_PARTITION).await.unwrap();
+        Self {
+            session,
+            insert_owner_query,
+            find_owner_by_partition,
+        }
     }
 
     pub async fn create(&self, owner: Owner) -> Result<()> {
-        let query = "INSERT INTO owners (owner_id, name, address) VALUES (?, ?, ?)";
-        let prepared = self.session.prepare(query).await?;
-
         self.session
-            .execute(&prepared, (owner.owner_id, owner.name, owner.address))
+            .execute(
+                &self.insert_owner_query,
+                (owner.owner_id, owner.name, owner.address),
+            )
             .await?;
 
         Ok(())
     }
 
     pub async fn find(&self, id: Uuid) -> Result<Owner> {
-        let query = "SELECT owner_id, name, address FROM owners WHERE owner_id = ?";
-
-        let mut prepared = self.session.prepare(query).await?;
-        prepared.set_page_size(1);
-
-        let result = self.session.execute(&prepared, (id, )).await?
+        let result = self
+            .session
+            .execute(&self.find_owner_by_partition, (id,))
+            .await?
             .rows_typed::<Owner>()?;
-
 
         if let Some(owner) = result.into_iter().next().transpose()? {
             return Ok(owner);
--- tests/owner_repository_test.rs
+++ tests/owner_repository_test.rs
@@ -1,23 +1,26 @@
+use std::str::FromStr;
 use std::sync::Arc;
 
 use anyhow::Result;
 use care_pet::cli::ServerConfig;
 use care_pet::database::new_session_with_keyspace;
+use care_pet::model::owner::Owner;
 use care_pet::repositories::owner_repository::OwnerRepository;
+use uuid::Uuid;
 
-use crate::common::setup;
-
-mod common;
 
 #[tokio::test]
 async fn owner_can_be_created() -> Result<()> {
     let session = new_session_with_keyspace(&ServerConfig::default()).await.expect("Connection refused.");
     let session = Arc::new(session);
 
-    let (owner) = setup(Arc::clone(&session)).await;
+    let owner = Owner {
+        owner_id: Uuid::from_str("03d61583-2f22-450f-8ae1-3c425e0761ad").unwrap(),
+        name: "Daniel Reis".to_string(),
+        address: "Flamengo St. 1337".to_string(),
+    };
 
     let owner_repository = OwnerRepository::new(session).await;
-
     owner_repository.create(owner).await?;
 
     Ok(())
